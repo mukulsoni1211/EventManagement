e6ff0f34c7b4f376747ab38633f61605559c2fb6
Ashrut Panchal <test@test.com>
2023-12-28 22:11:35 +0530

    Apply Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+
+
+  def authorize_request
+    header = request.headers['Authorization']
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+
+    @current_user
+  end
+
+  def current_user
+    @current_user ||= authorize_request
+  end
+
+  def not_found
+    render json: { error: 'not_found' }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    post 'login', to: "sessions#login"
+  end
 end
