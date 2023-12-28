d516b110374e2cfd08229d5f2eaae1ca5a960179
Ashrut Panchal <test@test.com>
2023-12-28 19:26:18 +0530

    Add booking model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :users, through: :bookings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :roles, join_table: :roles_users
   has_many :events
+  has_many :bookings
+  has_many :events, through: :bookings
 end
