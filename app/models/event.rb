30b8190eaf40c68f6fda7feb9cada43d01059524
Ashrut Panchal <test@test.com>
2023-12-29 23:59:30 +0530

    Add dependent destroy option

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   validates :title, :start_time, :end_time, presence: true
   after_update :notify_event_customers
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   has_and_belongs_to_many :roles, join_table: :roles_users
-  has_many :events
-  has_many :bookings
-  has_many :events
+  has_many :events, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   has_many :user_events, through: :bookings
   validates :email, presence: true, uniqueness: true
 
