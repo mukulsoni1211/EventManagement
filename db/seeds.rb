1aae09432f8c59ace77ac43b02231591e90b4a25
Ashrut Panchal <test@test.com>
2023-12-29 23:59:51 +0530

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+eo = Role.create(name: "EventOrganizer")
+c = Role.create(name: "Customer")
+
+organizer = User.create(
+  email: 'event@test.com',
+  name: 'Event Organizer',
+  password: '1234'
+)
+
+customer = User.create(
+  email: 'customer1@test.com',
+  name: 'Event Organizer',
+  password: '1234'
+)
+
+organizer.roles << eo
+customer.roles << c
+
+5.times { |i|
+  organizer.events.create(
+    title: "Event #{i + 1}",
+    desc: "Desc of event #{i + 1}",
+    start_time: DateTime.now + 5.day,
+    end_time: DateTime.now + 5.day + 5.hour,
+    total_ticket: 100
+  ) 
+}
