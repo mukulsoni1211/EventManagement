75aeab07f410f9a84c1160df7ce5c4884a2f3357
Ashrut Panchal <test@test.com>
2023-12-30 08:48:42 +0530

    update the migration

--- db/migrate/20231228124459_user_role_table.rb
+++ db/migrate/20231228124459_user_role_table.rb
@@ -1,8 +1,5 @@
 class UserRoleTable < ActiveRecord::Migration[7.1]
   def change
-    create_join_table :roles, :users do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :role, null: false, foreign_key: true
-    end
+    create_join_table :roles, :users
   end
 end
